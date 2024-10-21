222734524c1e1b410cf521f65011ea5d40215c4c
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2024-10-21 18:35:00 +0600

    Change production url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import UserGrid from "./components/UserGrid";
 import { useState, useEffect } from "react";
 
 
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "/api";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://127.0.0.1:5000/api" : "https://devfriendshub.up.railway.app/api";
 
 
 function App() {
